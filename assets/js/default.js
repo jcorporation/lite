f143af244f74911f16eb68a0b2af23412e249996
jcorporation <mail@jcgames.de>
2022-01-24 21:06:46 +0100

    Update

--- assets/js/default.js
+++ assets/js/default.js
@@ -4,8 +4,8 @@ const clickBoxes = document.getElementsByClassName('clickable');
 for (const box of clickBoxes) {
     const link = box.getElementsByTagName('a')[0];
     if (link) {
-        box.addEventListener('click', function(event) {
-            link.dispatchEvent(event);
+        box.addEventListener('click', function() {
+            link.click();
         }, false);
     }
 }
